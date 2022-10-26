7bcb3691280a378ac7e4a8a6e20be6b82f23070b
Yul3102 <hsy3102@gmail.com>
2022-10-25 23:36:23 -0400

    update

---
+++ polymorphism.ts
@@ -0,0 +1,33 @@
+interface SStorage<T> {
+    [key:string]:T
+}
+
+class LocalStorage <T>{
+    private storage: SStorage<T> = {};
+    set(key:string,value:T){
+        this.storage[key] = value;
+    }
+    remove(key:string){
+        delete this.storage[key];
+    }
+    get(key:string):T{
+        return this.storage[key];
+    }
+    clear(){
+        this.storage = {};
+    }
+}
+
+
+
+const stringsStorage = new LocalStorage<string>();
+
+stringsStorage.get("nico");
+
+const numbersStorage = new LocalStorage<number>();
+
+numbersStorage.set("nico",123);
+
+const booleanStorage = new LocalStorage<boolean>();
+booleanStorage.set("nico",true);
+
